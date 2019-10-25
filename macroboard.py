b69bfd08bca417feb4b500410bbc7bfd0878c5f6
Luke Davidson <luke@lukedavidson.xyz>
2019-10-24 23:03:00 -0700

    Broke up code into three functions rather than one

--- macroboard.py
+++ macroboard.py
@@ -10,6 +10,35 @@ dev.grab()
 ui = UInput()
 
 
+## write the key specified in the bind_dict
+def write_input(): 
+    for i in bind:
+        key_number = ecodes.ecodes[i]
+        ui.write(ecodes.EV_KEY, key_number, 1)
+        ui.syn()
+    for i in bind:
+        key_number = ecodes.ecodes[i]
+        ui.write(ecodes.EV_KEY, key_number, 0)
+        ui.syn()
+
+
+## check bind type and execute associated bind
+def execute_bind():
+    if key.keycode in bindings:
+        bind = bindings[key.keycode][1]
+        if bindings[key.keycode][0] == 'input':
+            write_input()
+        elif bindings[key.keycode][0] == 'web':
+            webbrowser.open(bind,new=2,autoraise=True)
+        elif bindings[key.keycode][0] == 'unassigned':
+            print('No acton assigned to ' + key.keycode)
+            subprocess.call(['notify-send', '-u', 'low', 'MacroBoard', 'No Action Assigned to ' + key.keycode])
+        else:
+            print('Key must be a web or input type. Key is currently set to the type: ' + bindings[key.keycode][0])
+    else:
+        print(key.keycode + ' is undefined')
+
+
 ## Loop to read input from device 
 for event in dev.read_loop():
     if event.type == ecodes.EV_KEY:
@@ -18,25 +47,4 @@ for event in dev.read_loop():
             if key.keycode == 'KEY_ESC':
                 quit()
             else:
-                if key.keycode in bindings:
-                    ## bind is assisgned the value of the second item in the list within the bindings values
-                    bind = bindings[key.keycode][1]
-                    if bindings[key.keycode][0] == 'input':
-                        ## write the key specified in the bind_dict
-                        for i in bind:
-                            key_number = ecodes.ecodes[i]
-                            ui.write(ecodes.EV_KEY, key_number, 1)
-                            ui.syn()
-                        for i in bind:
-                            key_number = ecodes.ecodes[i]
-                            ui.write(ecodes.EV_KEY, key_number, 0)
-                            ui.syn()
-                    elif bindings[key.keycode][0] == 'web':
-                        webbrowser.open(bind,new=2,autoraise=True)
-                    elif bindings[key.keycode][0] == 'unassigned':
-                        print('No acton assigned to ' + key.keycode)
-                        subprocess.call(['notify-send', '-u', 'low', 'MacroBoard', 'No Action Assigned to ' + key.keycode])
-                    else:
-                        print('Key must be a web or input type. Key is currently set to the type: ' + bindings[key.keycode][0])
-                else:
-                    print(key.keycode + ' is undefined')
+                execute_bind()
